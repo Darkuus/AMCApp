53ac9314709faa4651be940ac30f365cf76f343d
Darkuus <vitork42@gmail.com>
2020-06-30 20:15:40 -0300

    api

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     //baseURL: 'https://amc-api.herokuapp.com/',
-    baseURL: 'http://localhost:3333',
+    baseURL: 'https://amc-api.herokuapp.com/',
     responseType: 'json',
     headers: {
         'Content-Type': 'application/json'
